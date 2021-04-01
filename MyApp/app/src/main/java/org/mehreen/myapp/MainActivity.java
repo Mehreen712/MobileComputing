2a4a40462fe9d55cd212e65fd7d6bee5ff75a053
Mehreen Tariq <mehreentariq028@gmail.com>
2021-04-01 09:47:46 -0700

    3rd commit

--- MyApp/app/src/main/java/org/mehreen/myapp/MainActivity.java
+++ MyApp/app/src/main/java/org/mehreen/myapp/MainActivity.java
@@ -3,12 +3,16 @@ package org.mehreen.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear_layout);
+        setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.button4);
+        button = findViewById(R.id.button3);
+        button = findViewById(R.id.button);
     }
 }
