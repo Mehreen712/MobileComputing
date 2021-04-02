9c37e2fbcfbc17b89459489bf2541ad7c68c7a7c
Mehreen Tariq <mehreentariq028@gmail.com>
2021-04-02 05:04:02 -0700

    layouts

--- MyApp/app/src/main/java/org/mehreen/myapp/MainActivity.java
+++ MyApp/app/src/main/java/org/mehreen/myapp/MainActivity.java
@@ -3,16 +3,24 @@ package org.mehreen.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button;
+TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button=findViewById(R.id.button4);
-        button = findViewById(R.id.button3);
-        button = findViewById(R.id.button);
+       Button mybutton = findViewById(R.id.button3);
+       Button  newbutton = findViewById(R.id.button);
+       textview=findViewById(R.id.textView3);
+    }
+
+    public void FeelFreeToContactUs(View view) {
+        textview.setText("FeelFreeToContactUs");
     }
 }
